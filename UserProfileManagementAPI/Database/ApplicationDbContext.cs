039ff2bc258fbf2c6cfe015d51561c34865a81ad
Akash <akashveer98@gmai.com>
2021-04-04 11:41:52 +0530

    SQL Server DbContext added

---
+++ UserProfileManagementAPI/Database/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserProfileManagementAPI.Entities;
+
+namespace UserProfileManagementAPI.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationDbContext(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("ConnectionString"));
+        }
+        public DbSet<User> Users { get; }
+    }
+}
